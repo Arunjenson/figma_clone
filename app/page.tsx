6c3076839515c43b93e6f947fe423194d2ff849b
Arunjenson-SurveySparrow <arun.jenson@surveysparrow.com>
2024-02-10 18:05:51 +0530

    added shadcn and starter files

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import {CollaborativeApp} from './CollaborativeApp';
 
 export default function Page() {
   return (
-    <Room>
-      <CollaborativeApp />
-    </Room>
+  <h1 className='text-5xl text-white'>hello figma clone</h1>
   )
 }
