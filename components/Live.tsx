485c2b603ae482c2a6febb98f446b1b66aa80188
Arunjenson-SurveySparrow <arun.jenson@surveysparrow.com>
2024-03-03 00:08:48 +0530

    added live cursor chats

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,33 +1,71 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import LiveCursors from './cursor/LiveCursors'
 import { useMyPresence, useOthers } from '@/liveblocks.config'
+import CursorChat from './cursor/CursorChat';
+import { CursorMode } from '@/types/type';
 
 const Live = () => {
   const others = useOthers();
   const [{ cursor }, updateMyPresence] = useMyPresence() as any;
+  const [cursorState, setCursorState] = useState({
+    mode: CursorMode.Hidden
+  });
 
   const handlePointerMove = useCallback((event: React.PointerEvent) => {
     event.preventDefault();
     const x = event.clientX - event.currentTarget.getBoundingClientRect().x
     const y = event.clientY - event.currentTarget.getBoundingClientRect().y
-    updateMyPresence({cursor:{x,y}})
+    updateMyPresence({ cursor: { x, y } })
   }, [])
-  
+
   const handlePointerDown = useCallback((event: React.PointerEvent) => {
     const x = event.clientX - event.currentTarget.getBoundingClientRect().x
     const y = event.clientY - event.currentTarget.getBoundingClientRect().y
-    updateMyPresence({cursor:{x,y}})
+    updateMyPresence({ cursor: { x, y } })
   }, [])
-  
+
   const handlePointerLeave = useCallback((event: React.PointerEvent) => {
-    event.preventDefault();
-    updateMyPresence({cursor:null,message:null})
-  },[])
+    setCursorState({ mode: CursorMode.Hidden })
+    updateMyPresence({ cursor: null, message: null })
+  }, [])
+
+  useEffect(() => {
+    const onKeyUp = (e: KeyboardEvent) => {
+      if (e.key === '/') {
+        setCursorState({
+          mode: CursorMode.Chat,
+          previousMessage: null,
+          message: ''
+        })
+      }
+      else if (e.key === 'Escape') {
+        updateMyPresence({ message: '' })
+        setCursorState({
+          mode: CursorMode.Hidden
+        })
+      }
+    }
+    const onKeyDown = (e: KeyboardEvent) => {
+      if (e.key === ' /') {
+        e.preventDefault();
+
+      }
+    }
+    window.addEventListener('keydown', onKeyDown)
+    window.addEventListener('keyup', onKeyUp)
+    return () => {
+      window.removeEventListener('keydown', onKeyDown)
+      window.removeEventListener('keyup',onKeyUp)
+    }
+  }, [updateMyPresence])
   return (
     <div onPointerDown={handlePointerDown} onPointerLeave={handlePointerLeave} onPointerMove={handlePointerMove}
-    className="h-screen w-full flex justify-center items-center text-center"
+      className="h-screen w-full flex justify-center items-center text-center"
     >
       <h1 className='text-5xl text-white'>hello figma clone</h1>
+      {cursor && (
+        <CursorChat cursor={cursor} cursorState={cursorState} setCursorState={setCursorState} updateMyPresence={updateMyPresence} />
+      )}
       <LiveCursors others={others} />
     </div>
   )
--- components/cursor/Cursor.tsx
+++ components/cursor/Cursor.tsx
@@ -10,7 +10,14 @@ type Props = {
 const Cursor = ({color,x,y,message}:Props) => {
   return (
       <div className="pointer-events-none absolute top-0 left-0" style={{ transform: `translateX(${x}px) translateY(${y}px)` }}>
-          <CursorSVG color={color} />
+      <CursorSVG color={color} />
+      {message && (
+        <div className="absolute left-2 top-5 rounded-3xl px-4 py-2" style={{
+          backgroundColor:color
+        }}>
+          <p className="text-white whitespace-nowrap text-sm leading-relaxed">{message}</p>
+        </div>
+      )}
     </div>
   )
 }
