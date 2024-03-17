7400c1b3c9189ea70c68fa2250fcabd7f30f48ac
Arunjenson-SurveySparrow <arun.jenson@surveysparrow.com>
2024-03-17 19:34:29 +0530

    added flying reactions

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,9 +1,11 @@
 import React, { useCallback, useEffect, useState } from 'react'
 import LiveCursors from './cursor/LiveCursors'
-import { useMyPresence, useOthers } from '@/liveblocks.config'
+import { useBroadcastEvent, useEventListener, useMyPresence, useOthers } from '@/liveblocks.config'
 import CursorChat from './cursor/CursorChat';
-import { CursorMode, CursorState, Reaction } from '@/types/type';
+import { CursorMode, CursorState, Reaction, ReactionEvent } from '@/types/type';
 import ReactionSelector from './reaction/ReactionButton';
+import FlyingReaction from './reaction/FlyingReaction';
+import useInterval from '@/hooks/useInterval';
 
 const Live = () => {
   const others = useOthers();
@@ -12,7 +14,36 @@ const Live = () => {
     mode: CursorMode.Hidden
   });
 
-  const [reaction,setReaction] = useState<Reaction[]>([])
+  const [reaction, setReaction] = useState<Reaction[]>([])
+  const broadcast = useBroadcastEvent();
+  useInterval(() => {
+    setReaction((reaction)=>reaction.filter((r)=>r.timestamp > Date.now()-4000))
+  },1000)
+  useInterval(() => {
+    if (cursorState.mode === CursorMode.Reaction && cursorState.isPressed && cursor) {
+      setReaction((reactions) => reactions.concat([
+        {
+          point: { x: cursor.x, y: cursor.y },
+          value: cursorState.reaction,
+          timestamp: Date.now()
+        }]))
+      broadcast({
+        x: cursor.x,
+        y: cursor.y,
+        value:cursorState.reaction
+      })
+    }
+  }, 100)
+  
+  useEventListener((eventData) => {
+    const event = eventData.event as ReactionEvent;
+     setReaction((reactions) => reactions.concat([
+        {
+          point: { x: event.x, y: event.y },
+          value: event.value,
+          timestamp: Date.now()
+        }]))
+  })
   const handlePointerMove = useCallback((event: React.PointerEvent) => {
     event.preventDefault();
 
@@ -89,6 +120,16 @@ const Live = () => {
       className="h-screen w-full flex justify-center items-center text-center"
     >
       <h1 className='text-5xl text-white'>hello figma clone</h1>
+      {reaction.map((r) =>
+        <FlyingReaction
+          key={r.timestamp.toString()}
+          x={r.point.x}
+          y={r.point.y}
+          timestamp={r.timestamp}
+          value={r.value}
+
+        />
+      )}
       {cursor && (
         <CursorChat cursor={cursor} cursorState={cursorState} setCursorState={setCursorState} updateMyPresence={updateMyPresence} />
       )}
