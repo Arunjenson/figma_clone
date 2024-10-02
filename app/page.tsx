ea8be12f7ea4e4af33e670fb069caff5c360d3eb
Arunjenson-SurveySparrow <arun.jenson@surveysparrow.com>
2024-10-02 18:47:32 +0530

    added delete and reset functionalities

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,8 @@ import { useEffect, useRef, useState } from 'react';
 import { handleCanvaseMouseMove, handleCanvasMouseDown, handleCanvasMouseUp, handleCanvasObjectModified, handleResize, initializeFabric, renderCanvas } from '@/lib/canvas';
 import { ActiveElement } from '@/types/type';
 import { useMutation,useStorage } from '@/liveblocks.config';
+import { defaultNavElement } from '@/constants';
+import { handleDelete } from '@/lib/key-events';
 
 export default function Page() {
   const canvasRef = useRef<HTMLCanvasElement>(null)
@@ -36,9 +38,34 @@ export default function Page() {
     icon:""
   })
 
+const deleteAllShapes = useMutation(({storage})=>{
+      const canvasObjects = storage.get('canvasObjects');
+      if(!canvasObjects || canvasObjects.size === 0) return true;
+      for(const [key,value] of canvasObjects.entries()) {
+        canvasObjects.delete(key)
+      }
+      return canvasObjects.size===0
+},[])
+const deleteShapeFromStorage = useMutation(({storage},objectId)=>{
+  const canvasObjects = storage.get('canvasObjects');
+  canvasObjects.delete(objectId)
+},[])
   const handleActiveElement = (elem: ActiveElement) => {
     setActiveElement
       (elem);
+
+    switch(elem?.value){
+      case 'reset':
+        deleteAllShapes()
+        fabricRef.current?.clear()
+        setActiveElement(defaultNavElement)
+        break;
+      case 'delete': 
+        handleDelete(fabricRef.current as any, deleteShapeFromStorage) 
+        setActiveElement(defaultNavElement)
+      default:
+        break
+    }
     selectedShapeRef.current = elem?.value as string
   }
   
@@ -59,6 +86,9 @@ export default function Page() {
     window.addEventListener("resize", () => {
       handleResize({fabricRef})
     })
+    return () => {
+      canvas.dispose()
+    }
   },[])
 
   useEffect(() => {
